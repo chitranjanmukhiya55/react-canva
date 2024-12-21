6a81d2cbc554db70bbe095484b03d3fb19b4f2a9
chitranjanmukhiya55 <jyoti.kumari@jaiinfoway.com>
2024-12-21 11:04:30 +0530

    select editor file

--- src/views/DesignEditor/SelectEditor.tsx
+++ src/views/DesignEditor/SelectEditor.tsx
@@ -1,15 +1,20 @@
-import React from "react"
-import { Block } from "baseui/block"
-import { Button } from "baseui/button"
-import { DesignType } from "~/interfaces/DesignEditor"
-import useDesignEditorContext from "~/hooks/useDesignEditorContext"
-import Video from "~/components/Icons/Video"
-import Images from "~/components/Icons/Images"
-import Presentation from "~/components/Icons/Presentation"
+import React, { useEffect } from "react";
+import { Block } from "baseui/block";
+import { Button } from "baseui/button";
+import { DesignType } from "~/interfaces/DesignEditor";
+import useDesignEditorContext from "~/hooks/useDesignEditorContext";
+import Video from "~/components/Icons/Video";
+import Images from "~/components/Icons/Images";
+import Presentation from "~/components/Icons/Presentation";
 
 export default function () {
-  const [selectedEditor, setSelectedEditor] = React.useState<DesignType>("GRAPHIC")
-  const { setEditorType } = useDesignEditorContext()
+  const [selectedEditor, setSelectedEditor] = React.useState<DesignType>("GRAPHIC");
+  const { setEditorType } = useDesignEditorContext();
+
+  // Automatically trigger the continue action on load
+  useEffect(() => {
+    setEditorType("GRAPHIC");
+  }, [setEditorType]);
 
   return (
     <Block
@@ -47,50 +52,23 @@ export default function () {
             <Images size={34} />
             <Block>Graphic</Block>
           </Block>
-          {/* <Block
-            onClick={() => setSelectedEditor("PRESENTATION")}
-            $style={{
-              height: "180px",
-              width: "180px",
-              background: selectedEditor === "PRESENTATION" ? "#000000" : "rgb(231, 236, 239)",
-              color: selectedEditor === "PRESENTATION" ? "#ffffff" : "#333333",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              cursor: "pointer",
-              flexDirection: "column",
-              gap: "0.5rem",
-            }}
-          >
-            <Presentation size={36} />
-            <Block>Presentation</Block>
-          </Block> */}
-          {/* <Block
-            onClick={() => setSelectedEditor("VIDEO")}
-            $style={{
-              height: "180px",
-              width: "180px",
-              background: selectedEditor === "VIDEO" ? "#000000" : "rgb(231, 236, 239)",
-              color: selectedEditor === "VIDEO" ? "#ffffff" : "#333333",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              cursor: "pointer",
-              flexDirection: "column",
-              gap: "0.5rem",
-            }}
-          >
-            <Video size={36} />
-            <Block>Video</Block>
-          </Block> */}
         </Block>
-        <Block $style={{ display: "flex", alignItems: "center", justifyContent: "center", padding: "2rem 0" }}>
-          <Button style={{ width: "180px" }} onClick={() => setEditorType(selectedEditor)}>
+        <Block
+          $style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "2rem 0",
+          }}
+        >
+          <Button
+            style={{ width: "180px" }}
+            onClick={() => setEditorType(selectedEditor)}
+          >
             Continue
           </Button>
         </Block>
-
       </Block>
     </Block>
-  )
+  );
 }
